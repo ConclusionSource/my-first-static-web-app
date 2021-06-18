147345a6decdbfa9d5322155bc91137934ed41c3
David Smulders <DSmulders@conclusion.nl>
2021-06-18 10:57:22 +0200

    Working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}} (test2)</div>`,
 })
 export class AppComponent {
   value = 'World';
